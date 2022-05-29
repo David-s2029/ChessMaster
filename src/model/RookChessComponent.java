773e4794fe9bd2da9ba45e588484b5d928153148
David <12111314@mail.sustech.edu.cn>
2022-05-29 18:58:05 +0800

    ver0.1

--- src/model/RookChessComponent.java
+++ src/model/RookChessComponent.java
@@ -63,7 +63,7 @@ public class RookChessComponent extends ChessComponent {
 
     public RookChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor color, ClickController listener, int size,ChessComponent[][] chessComponents) {
         super(chessboardPoint, location, color, listener, size,chessComponents);
-        super.point=5;
+        super.point=4;
         if (color == ChessColor.WHITE) {
             super.name='r';
         } else if (color == ChessColor.BLACK) {
